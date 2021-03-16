b91363f2aee6836f8f2bf4bf573f58e7ff35c2b5
shishir <shishirkumar28@gmail.com>
2021-03-17 01:06:35 +0800

    Added test cases

--- src/test/java/com/example/restapi/controller/ApiControllerTest.java
+++ src/test/java/com/example/restapi/controller/ApiControllerTest.java
@@ -1,18 +1,15 @@
 package com.example.restapi.controller;
 
 import com.example.restapi.model.Product;
-import com.example.restapi.service.ProductRepoService;
-import com.example.restapi.service.ProductRepoServiceImpl;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,20 +21,57 @@ class ApiControllerTest {
     ApiController apiController;
 
     @Test
-    public void shouldReturnListOfProduct() {
+    public void shouldReturnListOfProductAfterAdding() {
         MockHttpServletRequest request = new MockHttpServletRequest();
-        assertFalse(apiController.getAllProduct().getBody().isEmpty());
+        Product product = new Product("test", 12.0);
+        apiController.addProduct(product);
+        ResponseEntity<List<Product>> responseEntity = apiController.getAllProduct();
+        assertEquals(responseEntity.getStatusCode(), HttpStatus.OK);
+        assertFalse(responseEntity.getBody().isEmpty());
+        assertEquals(responseEntity.getBody().get(0).getName(), product.getName());
+        assertEquals(responseEntity.getBody().get(0).getPrice(), product.getPrice());
     }
 
     @Test
     public void shouldReturnProductWithId() {
         UUID id = UUID.randomUUID();
         MockHttpServletRequest request = new MockHttpServletRequest();
-        assertEquals(apiController.getProductById(id).getStatusCode(), HttpStatus.OK);
+        assertEquals(apiController.getProductById(id).getStatusCode(), HttpStatus.NO_CONTENT);
     }
 
     @Test
     public void shouldAddProductId() {
         MockHttpServletRequest request = new MockHttpServletRequest();
+        Product product = new Product("test", 12.0);
+        ResponseEntity<Product> responseEntity = apiController.addProduct(product);
+        assertEquals(responseEntity.getStatusCode(), HttpStatus.CREATED);
+        assertEquals(responseEntity.getBody().getName(), product.getName());
+        assertEquals(responseEntity.getBody().getPrice(), product.getPrice());
+        assertFalse(Optional.of(responseEntity.getBody().getId()).isEmpty());
+
+    }
+
+    @Test
+    public void shouldUpdateProduct() {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        Product product = new Product("test", 12.0);
+        apiController.addProduct(product);
+        Product updatedProd = product;
+        updatedProd.setName("test2");
+        ResponseEntity<Product> responseEntity = apiController.updateProduct(product);
+        assertEquals(responseEntity.getStatusCode(), HttpStatus.OK);
+        assertEquals(responseEntity.getBody().getName(), updatedProd.getName());
+        assertEquals(responseEntity.getBody().getPrice(), updatedProd.getPrice());
+        assertEquals(responseEntity.getBody().getId(), product.getId());
+
+    }
+
+    @Test
+    public void shouldDeleteProductById() {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        Product product = new Product("test", 12.0);
+        ResponseEntity<Product> prd = apiController.addProduct(product);
+        ResponseEntity<Product> responseEntity = apiController.deleteProductById(prd.getBody().getId());
+        assertEquals(responseEntity.getStatusCode(), HttpStatus.OK);
     }
 }
